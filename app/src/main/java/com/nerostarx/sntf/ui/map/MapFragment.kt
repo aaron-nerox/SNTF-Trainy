416e27457bfe5ed77b1cad8e674baae5efc948cc
Nerostarx <35573892+NeroStarx@users.noreply.github.com>
2021-03-24 23:22:13 +0100

    finish: map fragment

--- app/src/main/java/com/nerostarx/sntf/ui/map/MapFragment.kt
+++ app/src/main/java/com/nerostarx/sntf/ui/map/MapFragment.kt
@@ -1,22 +1,24 @@
 package com.nerostarx.sntf.ui.map
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.libraries.places.api.Places
 import com.nerostarx.sntf.R
 
 class MapFragment : Fragment(), OnMapReadyCallback {
 
-    companion object {
-        fun newInstance() = MapFragment()
-    }
-
     private lateinit var viewModel: MapViewModel
+    private val API_KEY = "AIzaSyCvYZHnDSX4RfKZp-zsZ5s91-_2H-7Fk-E"
+    private lateinit var mapView: MapView
+    private lateinit var gMap:GoogleMap
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -25,10 +27,43 @@ class MapFragment : Fragment(), OnMapReadyCallback {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        mapView = requireActivity().findViewById(R.id.map_drag)
+        mapView.onCreate(savedInstanceState)
+        Places.initialize(requireContext(), API_KEY)
+        mapView.getMapAsync(this)
     }
 
     override fun onMapReady(p0: GoogleMap?) {
+        gMap = p0!!
+        val algiers = LatLng(36.7525, 3.04197)
+        gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(algiers, 10f))
 
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onPause() {
+        mapView.onPause()
+        super.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onDestroy() {
+        mapView.onDestroy()
+        super.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
     }
 
 }
